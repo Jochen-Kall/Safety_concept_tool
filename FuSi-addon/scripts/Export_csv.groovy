a497277d4faaa597bcbc8c03db1659ab381ddc41
Jochen Kall <jochen-Kall@gmx.de>
2021-01-30 20:18:41 +0100

    Add License information to scripts

--- FuSi-addon/scripts/Export_csv.groovy
+++ FuSi-addon/scripts/Export_csv.groovy
@@ -1,6 +1,12 @@
-// Not implemented yet.
-// Intended to create a .csv export of all the nodes ignoring clones suitable for input into industry requirement management tools like Doors, DNG, Polarion etc
-
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (C) 2021 Jochen Kall
+// This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 2.
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. 
+/*
+Export functionality for requirements Data as .csv file
+Duplicates are NOT exported 
+*/
 import javax.swing.*;
 
 // Dialog window for easy copy and pasting
--- FuSi-addon/scripts/Extract_Tag_mapping.groovy
+++ FuSi-addon/scripts/Extract_Tag_mapping.groovy
@@ -1,5 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0
+// Copyright (C) 2021 Jochen Kall
+// This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 2.
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. 
+/*
+Searches all files under source code monitoring for [ID_xxx] tags of all requirements nodes.
+Constructs and adds Github links in the details of nodes for each identified requirement tag in the monitored source code files.
+*/
 import javax.swing.*;
-
 // Dialog window for easy copy and pasting
 def showDialog(String content) {
     //create new dialog and set size
