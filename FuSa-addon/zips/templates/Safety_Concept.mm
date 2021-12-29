456edab81885b62f8712baa9ce927713a03405b1
Jochen Kall <jochen.kall.oss@googlemail.com>
2021-12-29 14:39:56 +0100

    Added get_req_parent function to the library, provides next non caption ancestor up the tree. Needed to make the Safety logic compatible with captions

--- FuSa-addon/zips/templates/Safety_Concept.mm
+++ FuSa-addon/zips/templates/Safety_Concept.mm
@@ -1,16 +1,19 @@
-<map version="freeplane 1.9.0">
-<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<map version="freeplane 1.9.8">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
 <attribute_registry SHOW_ATTRIBUTES="hide"/>
 <node TEXT="Safety concept" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_273763478" CREATED="1609081280555" MODIFIED="1640038805804"><hook NAME="MapStyle" zoom="0.912">
-    <properties show_icon_for_attributes="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" fit_to_viewport="false"/>
+    <properties fit_to_viewport="false" show_icon_for_attributes="false" show_note_icons="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" ID="ID_1965934285" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
 <font SIZE="24"/>
 <stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
-<stylenode LOCALIZED_TEXT="default" ID="ID_1609829545" ICON_SIZE="12 pt" COLOR="#000000" STYLE="fork">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1609829545" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#000000" STYLE="fork" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" MAX_WIDTH="10 cm" MIN_WIDTH="0 cm" VGAP_QUANTITY="2 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#808080" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="SOLID">
 <arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1609829545" STARTARROW="NONE" ENDARROW="DEFAULT"/>
-<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/html" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/html"/>
+<edge STYLE="bezier" COLOR="#808080" WIDTH="1" DASH="SOLID"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.details"/>
 <stylenode LOCALIZED_TEXT="defaultstyle.attributes">
@@ -260,7 +263,7 @@
 <icon BUILTIN="HW"/>
 <edge COLOR="#007c00"/>
 </stylenode>
-<stylenode TEXT="SW" STYLE="rectangle" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#0066ff">
+<stylenode TEXT="SW" ID="ID_1906404182" STYLE="rectangle" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#0066ff">
 <icon BUILTIN="SW"/>
 <edge COLOR="#007c00"/>
 </stylenode>
@@ -324,6 +327,10 @@
 <icon BUILTIN="AgPL_e"/>
 <edge COLOR="#007c00"/>
 </stylenode>
+<stylenode TEXT="Caption" ID="ID_1969913387" STYLE="rectangle" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#ffffff">
+<font SIZE="14" BOLD="true"/>
+<edge COLOR="#007c00"/>
+</stylenode>
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
@@ -351,14 +358,21 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="57" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="59" RULE="ON_BRANCH_CREATION"/>
 <attribute_layout NAME_WIDTH="97.5 pt" VALUE_WIDTH="97.5 pt"/>
 <attribute NAME="FuSi-Addon-version" VALUE="0.5" OBJECT="org.freeplane.features.format.FormattedNumber|0.5|#0.####"/>
 <attribute NAME="Enable Tainting" VALUE="false"/>
 <attribute NAME="ISO_26262_mode" VALUE="true"/>
 <attribute NAME="ISO_13849_mode" VALUE="false"/>
 <attribute NAME="ISO_25119_mode" VALUE="false"/>
-<node TEXT="Safety Goal 1" STYLE_REF="Requirement" POSITION="right" ID="ID_971613141" CREATED="1609106418278" MODIFIED="1620808984073" HGAP_QUANTITY="20 pt" VSHIFT_QUANTITY="3 pt">
+<node TEXT="Safety Goals" STYLE_REF="Caption" POSITION="right" ID="ID_471313066" CREATED="1640778811616" MODIFIED="1640784042363">
+<attribute NAME="Type" VALUE="Caption"/>
+<attribute NAME="Type_sc" VALUE="FSR"/>
+<attribute NAME="ASIL" VALUE="B"/>
+<attribute NAME="ASIL_sc" VALUE="B"/>
+<attribute NAME="Allocation" VALUE="Not Allocated"/>
+<attribute NAME="Shared" VALUE="= node.getCountNodesSharingContent()&gt;0"/>
+<node TEXT="Safety Goal 1" STYLE_REF="Requirement" ID="ID_971613141" CREATED="1609106418278" MODIFIED="1620808984073" HGAP_QUANTITY="20 pt" VSHIFT_QUANTITY="3 pt">
 <attribute_layout VALUE_WIDTH="100 pt"/>
 <attribute NAME="ASIL" VALUE="B"/>
 <attribute NAME="Type" VALUE="SG"/>
@@ -368,7 +382,7 @@
 <attribute NAME="Allocation_sc" VALUE=""/>
 <attribute NAME="Shared" VALUE="= node.getCountNodesSharingContent()&gt;0"/>
 </node>
-<node TEXT="Safety Goal 2" STYLE_REF="Requirement" POSITION="right" ID="ID_759203603" CREATED="1611077204015" MODIFIED="1620808993868">
+<node TEXT="Safety Goal 2" STYLE_REF="Requirement" ID="ID_759203603" CREATED="1611077204015" MODIFIED="1620808993868">
 <attribute_layout VALUE_WIDTH="100 pt"/>
 <attribute NAME="Type" VALUE="SG"/>
 <attribute NAME="ASIL" VALUE="A"/>
@@ -377,6 +391,7 @@
 <attribute NAME="Type_sc" VALUE="SG"/>
 <attribute NAME="Allocation_sc" VALUE=""/>
 <attribute NAME="Shared" VALUE="= node.getCountNodesSharingContent()&gt;0"/>
+</node>
 </node>
 <node TEXT="Architecture" POSITION="left" ID="ID_1988403535" CREATED="1609881073321" MODIFIED="1614814447007" HGAP_QUANTITY="77 pt" VSHIFT_QUANTITY="217.49999 pt">
 <edge COLOR="#0000ff"/>
